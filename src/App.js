c74632cee7d1adc2694f4843071d78c60107dae2
utsav <guptautsavkumar126@gmail.com>
2023-12-11 17:22:50 +0530

    firstReactProject

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Home=()=>{
+    let a=10
+    return <h1 style={{color:"red",textAlign:"center"}}>Home,{a}</h1>
+    
 }
 
-export default App;
+export default Home
---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button=({fruitName})=>{
+    return(
+        <div>
+            <button>{fruitName}</button>
+        </div>
+    )
+}
+
+export default Button;
